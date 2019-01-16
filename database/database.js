00ee98ad31c1b18e3cda142bda9b7c2c9b8c2f86
justplayit <free_roaming94@yahoo.com>
2019-01-16 21:09:32 +0200

    changed db password

--- database/database.js
+++ database/database.js
@@ -3,8 +3,8 @@ const mysql = require('mysql');
 var connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "root",
-    database: 'knbn-secure-db'
+    password: "knbn-secure-db",
+    database: 'knbn'
   });
 
 connection.connect(function(error){
