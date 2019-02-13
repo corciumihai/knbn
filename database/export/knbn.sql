b552e31a088d3839db33a002498c4250696b12b2
Mihai Corciu <free_roaming94@yahoo.com>
2019-02-13 03:52:13 +0200

    Add files via upload

--- database/export/knbn.sql
+++ database/export/knbn.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Feb 12, 2019 at 09:33 AM
+-- Generation Time: Feb 13, 2019 at 01:49 AM
 -- Server version: 5.6.34-log
 -- PHP Version: 7.1.5
 
@@ -38,7 +38,17 @@ CREATE TABLE IF NOT EXISTS `categories` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`),
   UNIQUE KEY `unique on name and project` (`project`,`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `project`, `name`) VALUES
+(3, 4, 'Altele'),
+(4, 4, 'Arhitectură'),
+(2, 4, 'Hardware'),
+(1, 4, 'Software');
 
 -- --------------------------------------------------------
 
@@ -62,14 +72,19 @@ CREATE TABLE IF NOT EXISTS `components` (
   KEY `category` (`category`),
   KEY `release_null` (`releaseID`),
   KEY `owner` (`owner`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `components`
 --
 
 INSERT INTO `components` (`id`, `name`, `project`, `category`, `description`, `releaseID`, `priority`, `owner`) VALUES
-(1, 'asda', 3, NULL, '', NULL, 'low', 'free_roaming94@yahoo.com');
+(1, 'asda', 3, NULL, '', NULL, 'low', 'free_roaming94@yahoo.com'),
+(2, 'Securitatea aplicației', 4, 1, '<p>Implementarea securității totale a aplicației</p>', 1, 'high', 'corciumihai@yahoo.com'),
+(3, 'Arhitectură', 4, 4, '<p>Definește arhitectura unui proiect.</p>', 6, 'low', 'free_roaming94@yahoo.com'),
+(4, 'Bază de date', 4, 1, '<p>Configurează baza de date</p>', 6, 'low', 'free_roaming94@yahoo.com'),
+(5, 'Server', 4, 1, '<p>Implementarea serverului</p>', 5, 'low', 'corciumihai@yahoo.com'),
+(6, 'Client ReactJS', 4, 1, '<p>Implementarea clientului bazată pe tehnologia ReactJS</p>', 1, 'low', 'corciumihai@yahoo.com');
 
 -- --------------------------------------------------------
 
@@ -87,14 +102,15 @@ CREATE TABLE IF NOT EXISTS `projects` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `projects`
 --
 
 INSERT INTO `projects` (`id`, `name`, `description`, `startDate`, `wip`) VALUES
-(3, 'Playground', '', '2019-02-10', 3);
+(3, 'Playground', '', '2019-02-10', 3),
+(4, 'KBNB - Principiile metodologiei kanban în format electronic', '<p>Proiectul de licență propus de Mihai Corciu, coordonator științific de Lector, Dr. Cristian Traian Vidrașcu.</p>', '2019-02-12', 2);
 
 -- --------------------------------------------------------
 
@@ -111,7 +127,18 @@ CREATE TABLE IF NOT EXISTS `releases` (
   UNIQUE KEY `id` (`id`),
   UNIQUE KEY `name` (`name`,`project`) USING BTREE,
   KEY `project` (`project`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `releases`
+--
+
+INSERT INTO `releases` (`id`, `name`, `project`) VALUES
+(7, 'sada', 4),
+(6, 'v.0.0.1', 4),
+(5, 'v.0.0.2', 4),
+(1, 'v.1.0.0', 4),
+(4, 'v.1.0.1', 4);
 
 -- --------------------------------------------------------
 
@@ -147,15 +174,17 @@ CREATE TABLE IF NOT EXISTS `reports` (
   KEY `reporter` (`reporter`),
   KEY `category` (`category`),
   KEY `component` (`component`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `reports`
 --
 
 INSERT INTO `reports` (`id`, `component`, `name`, `description`, `startDate`, `dueDate`, `category`, `reporter`, `assignee`, `estimation`, `priority`, `lane`, `testSteps`, `expected`, `observed`, `project`, `releaseID`, `blocked`) VALUES
-(5, 1, 'sadad', '', '2019-02-12', '2019-02-28', NULL, 'free_roaming94@yahoo.com', 'free_roaming94@yahoo.com', 0, 'high', 'backlog', '', '', '', 3, NULL, 14),
-(6, 1, 'asdad', '', '2019-02-12', '2019-02-12', NULL, 'free_roaming94@yahoo.com', 'free_roaming94@yahoo.com', 0, 'high', 'backlog', '', '', '', 3, NULL, 15);
+(5, 1, 'Test 2', '', '2019-02-12', '2019-02-28', NULL, 'free_roaming94@yahoo.com', 'free_roaming94@yahoo.com', 0, 'high', 'in_progress', '', '', '', 3, NULL, NULL),
+(6, 1, 'Test 1', '', '2019-02-12', '2019-02-12', NULL, 'free_roaming94@yahoo.com', 'free_roaming94@yahoo.com', 0, 'high', 'backlog', '', '', '', 3, NULL, NULL),
+(10, 4, 'Zilele limită nu pot fi salvate', '<p>Baza de date returnează eroare când se încearcă inserarea zilei limită</p>', '2019-02-12', '2019-03-06', 1, 'free_roaming94@yahoo.com', 'corciumihai@yahoo.com', 5, 'high', 'done', '<ol><li>Creare un tichet nou</li><li>Eroare apare</li></ol>', '<p>Să nu fie eroare</p>', '<p>Mesaj de eroare în pagină</p>', 4, 1, NULL),
+(11, 6, 'Mesajele de succes nu apar', '', '2019-02-13', '2019-02-13', NULL, 'free_roaming94@yahoo.com', 'corciumihai@yahoo.com', 0, 'high', 'done', '', '', '', 4, NULL, 37);
 
 -- --------------------------------------------------------
 
@@ -194,7 +223,14 @@ CREATE TABLE IF NOT EXISTS `report_worklogs` (
   UNIQUE KEY `report_worklogs_id` (`id`),
   KEY `owner` (`owner`),
   KEY `report` (`report`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `report_worklogs`
+--
+
+INSERT INTO `report_worklogs` (`id`, `comment`, `hours`, `owner`, `report`, `created`) VALUES
+(1, '<p>Toate datele calendaristice sunt supuse conversiei acum</p>', 2, 'free_roaming94@yahoo.com', 10, '2019-02-12');
 
 -- --------------------------------------------------------
 
@@ -225,15 +261,33 @@ CREATE TABLE IF NOT EXISTS `tickets` (
   KEY `reporter` (`reporter`),
   KEY `category` (`category`),
   KEY `component` (`component`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tickets`
 --
 
 INSERT INTO `tickets` (`id`, `component`, `name`, `description`, `startDate`, `dueDate`, `category`, `reporter`, `assignee`, `estimation`, `priority`, `lane`, `project`, `releaseID`) VALUES
-(14, 1, 'asda', '', '2019-02-12', '2019-02-24', NULL, 'free_roaming94@yahoo.com', 'free_roaming94@yahoo.com', 0, 'low', 'backlog', 3, NULL),
-(15, 1, 'aadasd', '', '2019-02-12', '2019-02-12', NULL, 'free_roaming94@yahoo.com', 'free_roaming94@yahoo.com', 0, 'low', 'backlog', 3, NULL);
+(16, 2, 'Generează certificatul SSL', '<p>Genereaza certificatele Secure Socket Layer</p>', '2019-02-12', '2019-02-14', 1, 'free_roaming94@yahoo.com', 'corciumihai@yahoo.com', 2, 'medium', 'closed', 4, 6),
+(17, 2, 'Instalează biblioteca bCrypt', '<p>Parolele utilizatorilor vor fi criptate cu ajutorul acestei biblioteci.</p>', '2019-02-12', '2019-02-14', 1, 'free_roaming94@yahoo.com', 'corciumihai@yahoo.com', 20, 'high', 'closed', 4, 5),
+(22, 2, 'Configurează mecanismul de autentificare JWT', '<p>Instalează biblioteca JSON Web Token</p>', '2019-02-12', '2019-03-01', 1, 'mobile_user@yahoo.com', 'corciumihai@yahoo.com', 15, 'medium', 'done', 4, 1),
+(23, 3, 'Arhitectură proiect', '<p>Creează arhitectura unui proiect/tabelă.</p>', '2019-02-12', '2019-03-01', 4, 'free_roaming94@yahoo.com', 'free_roaming94@yahoo.com', 20, 'high', 'closed', 4, 6),
+(24, 3, 'Arhitectură modul', '<p>Creeză arhitectura unui module/coloane.</p>', '2019-02-12', '2019-03-01', 4, 'free_roaming94@yahoo.com', 'free_roaming94@yahoo.com', 10, 'low', 'done', 4, 6),
+(25, 3, 'Arhitectură tichet', '<p>Descrierea arhitecturii unui modul</p>', '2019-02-12', '2019-03-06', 4, 'free_roaming94@yahoo.com', 'free_roaming94@yahoo.com', 5, 'medium', 'in_progress', 4, 6),
+(26, 3, 'Arhitectură raport problemă', '<p>Descrie arhitectura unui raport problemă</p>', '2019-02-12', '2019-03-05', 4, 'free_roaming94@yahoo.com', 'free_roaming94@yahoo.com', 5, 'low', 'closed', 4, 6),
+(27, 4, 'Configureaza tabelul Utilizatorilor', '', '2019-02-12', '2019-03-07', 1, 'free_roaming94@yahoo.com', 'corciumihai@yahoo.com', 1, 'high', 'closed', 4, 6),
+(28, 5, 'Crează ruta și mecanism autentificare', '', '2019-02-12', '2019-02-12', 1, 'free_roaming94@yahoo.com', 'free_roaming94@yahoo.com', 0, 'high', 'in_progress', 4, 5),
+(29, 5, 'Implementează ruta de înregistrare', '', '2019-02-12', '2019-02-12', 1, 'free_roaming94@yahoo.com', 'free_roaming94@yahoo.com', 0, 'high', 'done', 4, 5),
+(30, 5, 'Implementeă ruta de deconectare', '', '2019-02-12', '2019-02-12', 1, 'free_roaming94@yahoo.com', 'corciumihai@yahoo.com', 0, 'low', 'done', 4, 5),
+(31, 6, 'Pagină de creare Proiect', '', '2019-02-12', '2019-03-03', 1, 'free_roaming94@yahoo.com', 'free_roaming94@yahoo.com', 0, 'low', 'in_progress', 4, 5),
+(32, 6, 'Pagină de create Modul', '', '2019-02-12', '2019-02-12', NULL, 'free_roaming94@yahoo.com', 'corciumihai@yahoo.com', 0, 'low', 'backlog', 4, NULL),
+(33, 6, 'Pagină de creare Tichet', '', '2019-02-12', '2019-02-12', NULL, 'free_roaming94@yahoo.com', 'free_roaming94@yahoo.com', 0, 'high', 'done', 4, NULL),
+(34, 6, 'Pagină de creare Raport problemă', '', '2019-02-12', '2019-02-12', NULL, 'free_roaming94@yahoo.com', 'free_roaming94@yahoo.com', 0, 'medium', 'backlog', 4, NULL),
+(35, 6, 'Pagină de editare Proiect', '', '2019-02-12', '2019-02-12', NULL, 'free_roaming94@yahoo.com', 'free_roaming94@yahoo.com', 0, 'high', 'in_progress', 4, NULL),
+(36, 6, 'Pagină de editare Modul', '', '2019-02-12', '2019-02-12', NULL, 'free_roaming94@yahoo.com', 'free_roaming94@yahoo.com', 0, 'low', 'in_progress', 4, NULL),
+(37, 6, 'Pagină de editare Tichet', '', '2019-02-12', '2019-02-12', NULL, 'free_roaming94@yahoo.com', 'free_roaming94@yahoo.com', 2, 'low', 'backlog', 4, NULL),
+(38, 6, 'Pagină de editare Raport problemă', '', '2019-02-13', '2019-02-12', 1, 'free_roaming94@yahoo.com', 'corciumihai@yahoo.com', 2, 'medium', 'closed', 4, 6),
+(39, 6, 'Mecanism eliminare elemente', '', '2019-02-13', '2019-02-13', 1, 'free_roaming94@yahoo.com', 'free_roaming94@yahoo.com', 2, 'medium', 'backlog', 4, 1);
 
 -- --------------------------------------------------------
 
@@ -253,7 +307,17 @@ CREATE TABLE IF NOT EXISTS `tickets_worklogs` (
   UNIQUE KEY `id` (`id`),
   KEY `ticket` (`ticket`),
   KEY `owner` (`owner`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tickets_worklogs`
+--
+
+INSERT INTO `tickets_worklogs` (`id`, `comment`, `hours`, `ticket`, `owner`, `created`) VALUES
+(1, '<p>Certificate generate și instalate.</p>', 2, 16, 'free_roaming94@yahoo.com', '2019-02-12'),
+(2, '<p>bCrypt up-and-running</p>', 15, 17, 'free_roaming94@yahoo.com', '2019-02-12'),
+(3, '<p>JWT este configurat pe server.</p>', 3, 22, 'free_roaming94@yahoo.com', '2019-02-12'),
+(4, '<p>Tabel creat</p>', 1, 27, 'free_roaming94@yahoo.com', '2019-02-12');
 
 -- --------------------------------------------------------
 
@@ -271,7 +335,7 @@ CREATE TABLE IF NOT EXISTS `ticket_comments` (
   UNIQUE KEY `id` (`id`),
   KEY `ticket` (`ticket`),
   KEY `owner` (`owner`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
